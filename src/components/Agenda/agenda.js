77883cf58b0d94d38d31d48a1768c0a2a1a9780c
KauanCleuton <kauancleuton733@gmail.com>
2023-10-20 00:26:44 -0300

    Novo Codigo

--- src/components/Agenda/agenda.js
+++ src/components/Agenda/agenda.js
@@ -1,51 +1,67 @@
 import { useConfirm } from "@/context/BtnContext";
-import { Button} from "@mui/material";
+import { Button } from "@mui/material";
 import { useState } from "react";
 import { ModalConfirm } from "../Modal";
 
 export const Agenda = () => {
-    const [open, setOpen] = useState(false)
-    const BtnContext = useConfirm();
-    const handleOpen = () => {
-        setOpen(!open)
-    }
+  const [open, setOpen] = useState(false);
+  const [selectedItemIndex, setSelectedItemIndex] = useState(null);
+  const BtnContext = useConfirm();
 
+  const handleOpen = (index) => {
+    setSelectedItemIndex(index);
+    setOpen(true);
+  };
 
-    return (
-        <div style={{
-            display: 'flex',
-            width: 'auto',
-            height: '300px',
-            color: 'white',
-            fontWeight: 'bold',
-            fontSize: '20px',
-            padding: '30px',
-            gap: '40px'
-        }}>
-            {BtnContext.confirm.map((item, index) => (
-                <div key={index} style={{
-                    display: 'flex',
-                    flexDirection: 'column'
-                }}>
-                    <ul style={{
-                        flex: 1,
-                        display: 'flex',
-                        alignItems: 'center',
-                        padding: '10px',
-                        gap: '30px',
-                        flexDirection: 'column'
-                    }}>
-                        <li>{item.title}</li>
-                        <Button onClick={handleOpen} variant={`${item.confirmed ? 'contained' : 'outlined'}`}>
-                            {item.confirmed ? 'CONFIRMADO' : 'VER DETALHES'}
-                        </Button>
-                    </ul>
-                    <ModalConfirm img={item.img} index={index} text={item.text} title={item.title}/>
-                </div>
+  const handleClose = () => {
+    setOpen(false);
+  };
 
-            ))}
-            
-
+  return (
+    <div
+      style={{
+        display: "flex",
+        width: "auto",
+        height: "300px",
+        color: "white",
+        fontWeight: "bold",
+        fontSize: "20px",
+        padding: "30px",
+        gap: "40px",
+      }}
+    >
+      {BtnContext.confirm.map((item, index) => (
+        <div key={index} style={{ display: "flex", flexDirection: "column" }}>
+          <ul
+            style={{
+              flex: 1,
+              display: "flex",
+              alignItems: "center",
+              padding: "10px",
+              gap: "30px",
+              flexDirection: "column",
+            }}
+          >
+            <li>{item.title}</li>
+            <Button
+              onClick={() => handleOpen(index)}
+              variant={`${item.confirmed ? "contained" : "outlined"}`}
+            >
+              {item.confirmed ? "CONFIRMADO" : "VER DETALHES"}
+            </Button>
+          </ul>
         </div>
-    );
+      ))}
+      {selectedItemIndex !== null && (
+        <ModalConfirm
+          img={BtnContext.confirm[selectedItemIndex].img}
+          index={selectedItemIndex}
+          text={BtnContext.confirm[selectedItemIndex].text}
+          title={BtnContext.confirm[selectedItemIndex].title}
+          open={open}
+          onClose={handleClose}
+        />
+      )}
+    </div>
+  );
 };
--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,67 +1,57 @@
-import React from 'react';
-import Button from '@mui/material/Button';
-import Modal from '@mui/material/Modal';
-import Box from '@mui/material/Box';
-import { useConfirm } from '@/context/BtnContext';
+import React from "react";
+import Button from "@mui/material/Button";
+import Modal from "@mui/material/Modal";
+import Box from "@mui/material/Box";
+import { useConfirm } from "@/context/BtnContext";
 
-export const ModalConfirm = ({ img, title, text, index }) => {
+export const ModalConfirm = ({ img, title, text, index, open, onClose }) => {
   const confirm = useConfirm();
-  const [open, setOpen] = React.useState(false);
-
-  const handleOpen = () => {
-    setOpen(true);
-  };
 
   const handleConfirmar = () => {
     confirm.handleConfirm(index);
-    setOpen(false);
-  };
-
-  const handleClose = () => {
-    setOpen(false);
+    onClose();
   };
 
   return (
-    <div>
-      <Button variant="contained" color="primary" onClick={handleOpen}>
-        Abrir Modal
-      </Button>
-      <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="simple-modal-title"
-        aria-describedby="simple-modal-description"
+    <Modal
+      open={open}
+      onClose={onClose}
+      aria-labelledby="simple-modal-title"
+      aria-describedby="simple-modal-description"
+    >
+      <Box
+        sx={{
+          position: "absolute",
+          display: "flex",
+          flexDirection: "column",
+          gap: "30px",
+          width: 400,
+          bgcolor: "background.paper",
+          border: "2px solid #000",
+          boxShadow: 24,
+          p: 2,
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+          color: "#004ABB",
+        }}
       >
-        <Box
-          sx={{
-            position: 'absolute',
-            display: 'flex',
-            flexDirection: 'column',
-            gap: '30px',
-            width: 400,
-            bgcolor: 'background.paper',
-            border: '2px solid #000',
-            boxShadow: 24,
-            p: 2,
-            top: '50%',
-            left: '50%',
-            transform: 'translate(-50%, -50%)',
-            color: '#004ABB',
+        <img
+          src={img}
+          alt={title}
+          style={{
+            width: "200px",
+            objectFit: "cover",
+            height: "200px",
+            margin: "0 auto",
+            borderRadius: "20px",
+            border: "2px solid #212121",
           }}
-        >
-          <img src={img} alt={title} style={{
-            width: '200px',
-            objectFit: 'cover',
-            height: '200px',
-            margin: '0 auto',
-            borderRadius: '20px',
-            border: '2px solid #212121'
-          }} />
-          <h2 id="simple-modal-title">{title}</h2>
-          <p id="simple-modal-description">{text}</p>
-          <Button onClick={handleConfirmar}>CONFIRMAR PRESENÇA</Button>
-        </Box>
-      </Modal>
-    </div>
+        />
+        <h2 id="simple-modal-title">{title}</h2>
+        <p id="simple-modal-description">{text}</p>
+        <Button onClick={handleConfirmar}>CONFIRMAR PRESENÇA</Button>
+      </Box>
+    </Modal>
   );
 };
